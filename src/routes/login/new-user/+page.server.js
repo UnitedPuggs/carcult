58b4e4863d318f0f5833caaff7ca3c5e2216294f
UnitedPuggs <edpoulsonv@hotmail.com>
2023-09-19 19:35:11 -0700

    fix: another url

--- src/routes/login/new-user/+page.server.js
+++ src/routes/login/new-user/+page.server.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({ parent }) {
     const { session } = await parent();
-    const req = await fetch(`http://localhost:5173/api/login?email=${session.user.email}`);
+    const req = await fetch(`https://carcult-git-mvp-unitedpuggs.vercel.app/api/login?email=${session.user.email}`);
     const res = await req.json();
     let username = res[0].username;
     if(username != null && username != '') {
