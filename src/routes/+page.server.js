138554c80627e840eb0a989cda969a5f81234956
UnitedPuggs <edpoulsonv@hotmail.com>
2023-09-19 19:34:34 -0700

    fix: url in fetch

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ export async function load({ parent }) {
     const { session } = await parent()
     
     if(session) {
-        const req = await fetch(`http://localhost:5173/api/login?email=${session.user.email}`);
+        const req = await fetch(`https://carcult-git-mvp-unitedpuggs.vercel.app/api/login?email=${session.user.email}`);
         const res = await req.json();
         session.user.displayname = res[0].username;
     }
