b78e204c46fb07208aa12583816d6229a5fdeb68
UnitedPuggs <edpoulsonv@hotmail.com>
2023-09-19 23:40:17 -0700

    trying to get replace to work

--- src/routes/api/login/update_username/+server.js
+++ src/routes/api/login/update_username/+server.js
@@ -2,10 +2,11 @@ import { supabase } from '$lib/supabase.js'
 
 export async function PATCH({ request }) {
     const { email, username } = await request.json();
+    let cleaned_user = username.replace(' ', '');
 
     const { error } = await supabase
     .from('users')
-    .update({username: username})
+    .update({username: cleaned_user})
     .eq('email', email);
 
     if(error)
