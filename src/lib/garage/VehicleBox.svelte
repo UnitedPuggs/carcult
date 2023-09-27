3fd90fcb9d7d008f0458fef4c4224d6c0a565a25
UnitedPuggs <edpoulsonv@hotmail.com>
2023-09-27 00:38:45 -0700

    feat: can now delete vehicles

--- src/lib/garage/VehicleBox.svelte
+++ src/lib/garage/VehicleBox.svelte
@@ -1,17 +1,41 @@
 <script>
+    import { invalidateAll } from '$app/navigation';
     export let main_image;
     export let vehicle_name;
     export let desc;
     export let info_id;
+    let edit_mode = false;
+
+    async function toggle_edit() {
+        edit_mode = !edit_mode
+    }
+
+    async function delete_vehicle(id) {
+        await fetch('/api/garage/delete_vehicle', {
+            method: "DELETE",
+            body: JSON.stringify({id: id})
+        })
+        edit_mode = false;
+        invalidateAll();
+    }
 
     if(main_image.includes("undefined"))
         main_image = "https://knnxtkccpetpqxmvcxmu.supabase.co/storage/v1/object/public/garage_photos/Hulk_Hogan.jpg"
 </script>
 
-<div class="flex flex-row flex-wrap border-2 border-white">
+<div class="flex flex-row flex-wrap border-2 border-white" id={info_id}>
     <img src={main_image} alt="cars" class="border-2 border-white w-[32rem] sm:h-52 md:h-auto md:max-h-[296px] object-cover">
-    <div class="">
-        <span class="text-2xl font-bold md:pr-32 md:pl-4">{vehicle_name}</span>
+    <div class="grow"> <!-- Not too sure why, but removing this grow breaks the grow on the grandchild div -->
+        <div class="flex flex-row">
+            <span class="text-2xl font-bold md:pr-32 md:pl-4">{vehicle_name}</span>
+            <div class="flex flex-col grow"> 
+                <button class="flex-row-reverse justify-end mr-2 mt-2 ml-auto hover:opacity-75" on:click={toggle_edit}>edit</button>
+                {#if edit_mode}
+                    <button class="flex-row-reverse justify-end mr-2 ml-auto hover:opacity-75">save</button>
+                    <button class="flex-row-reverse justify-end mr-2 ml-auto hover:opacity-75 -mb-12" on:click={() => delete_vehicle(info_id)}>delete</button>
+                {/if}
+            </div>
+        </div>
         {#if desc} 
             <p class="md:pl-4">{desc}</p>
         {/if}
--- src/routes/garage/[slug]/add-car/+page.svelte
+++ src/routes/garage/[slug]/add-car/+page.svelte
@@ -3,6 +3,14 @@
     let car; //Do I need this variable? Kinda forgot
     let file;
 
+    const uploaded_file = (e) => {
+        let img = e.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(img);
+        reader.onload = e => {
+            file = e.target.result;
+        }
+    }
 </script>
 
 <svelte:head>
@@ -13,9 +21,16 @@
 <div>
     <div class="flex flex-col justify-center items-center mt-4">
         <form class="flex flex-col" method="POST" enctype="multipart/form-data">
-            <input type="text" class="text-black py-1 px-0.5" placeholder="car here" id="car" name="car" required>
-            <input type="file" id="file" name="file" bind:value={file}> <!-- Might need some styling on this bad boy -->
-            <textarea class="text-black" id="desc" name="desc" placeholder="description here"></textarea>
+            <input type="text" class="text-black py-1 px-0.5 rounded-sm" placeholder="car here" id="car" name="car" required>
+            <label class="my-2">
+                <input type="file" id="file" name="file" bind:value={file} class="hidden" on:change={(e) => uploaded_file(e)} /> <!-- Might need some styling on this bad boy -->            
+                {#if file}
+                    <img src={file} alt="upload your car" class="border-4 border-white w-[512px] h-[296px] object-cover cursor-pointer"/>
+                {:else}
+                    <img src="/assets/image_upload.png" alt="upload your car" class="border-4 border-white w-[512px] h-[296px] object-none cursor-pointer"/>
+                {/if}
+            </label>
+            <textarea class="text-black rounded-sm" id="desc" name="desc" placeholder="description here"></textarea>
             <input type="submit" value="add" class="hover:opacity-75 cursor-pointer">
         </form>
     </div>
