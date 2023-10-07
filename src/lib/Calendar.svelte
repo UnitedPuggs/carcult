d76ee76eb03fcbdeab11a5fbc375e77567ac4e0f
Eddie <48199805+UnitedPuggs@users.noreply.github.com>
2023-10-06 18:40:54 -0700

    wip: added days from previous month to fill in calendarevent comps

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -6,13 +6,28 @@
     const date = new Date("2023-09-01");
     const curr_month = date.toLocaleString(undefined, { month: 'long' })
     const days = daysInMonth(date.getFullYear(), date.getMonth() + 1)
+
+    let first_day = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-01").getDay();
+    let prev_month_days = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); 
+
+    let calendar_days = []
+
+    for(let i = first_day; i > 0; --i) {
+        calendar_days.push(prev_month_days - (i - 1));
+    }
+
+    for(let i = 1; i <= days; ++i) {
+        calendar_days.push(i);
+    }
+
+    console.log(calendar_days)
 </script>
 
 <div class="justify-center items-center text-center mt-10 text-white">
     <div class="flex flex-row justify-center items-center text-center">
         <button>&lt;-</button>
             <h1 class="text-2xl p-2">{curr_month}</h1>
-        <button>-></button>
+        <button>-&gt;</button>
     </div>
     <section class="grid grid-cols-7 grid-rows-1 max-w-[108rem] mx-auto border border-white">
         <span>Sunday</span>
@@ -24,8 +39,8 @@
         <span>Saturday</span>
     </section>
     <div class="grid grid-cols-7 gap-[0.5px] mx-24 border-2 border-white p-1 rounded-sm">
-        {#each {length: days} as _, i}
-            <CalendarEvents month={curr_month} day={i+1} date={date}/>
+        {#each calendar_days as day}
+            <CalendarEvents month={curr_month} day={day} date={date}/>
         {/each}
     </div>
 </div>
--- src/lib/CalendarEvents.svelte
+++ src/lib/CalendarEvents.svelte
@@ -1,13 +1,13 @@
 <script>
     export let month;
     export let day;
-    export let date;
+    //export let date;
 
-    let curr_date = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + day).toLocaleString('en-us', { weekday: 'long' });
+    //let curr_date = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + day).toLocaleString('en-us', { weekday: 'long' });
 </script>
 
 <div class="hover:opacity-75 border border-white flex flex-col h-32">
-    <h1>{curr_date}</h1>
+    <!--<h1>{curr_date}</h1>-->
     <span>{day}</span>
     <button>test</button>
 </div>
