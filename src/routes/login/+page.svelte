87754adec0d34d367f29bf6cd34814026181e714
UnitedPuggs <edpoulsonv@hotmail.com>
2023-10-30 16:51:30 -0700

    feat: issue #18 resolved

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,8 @@
 
 {#if !$page.data.session?.user}
 <div class="flex flex-col justify-center items-center mt-10">
-    <button class="border-2 rounded-lg border-white p-2 hover:opacity-75" on:click={() => signIn("google")}>log in with Google</button>
-    <button class="border-2 rounded-lg border-purple-300 py-2 px-[4px] mt-4 hover:opacity-75" on:click={() => signIn("discord")}>log in with Discord</button>
+    <button class="border-2 rounded-md border-white p-2 hover:opacity-75" on:click={() => signIn("google")}>log in with Google</button>
+    <button class="border-2 rounded-md border-purple-300 py-2 px-[4px] mt-4 hover:opacity-75" on:click={() => signIn("discord")}>log in with Discord</button>
 </div>
 {:else}
     <div>
--- src/routes/meets/[month]/[year]/+page.server.js
+++ src/routes/meets/[month]/[year]/+page.server.js
@@ -2,15 +2,25 @@ import { supabase } from '$lib/supabase.js'
 
 export async function load({ params }) {
     let date_str;
-    if(params.month < 10)
-        date_str = params.year + "-0" + params.month
-    else
-        date_str = params.year + "-" + params.month
+    let prev_date_str;
 
+    if(params.month < 10) {
+        date_str = params.year + "-0" + params.month
+    } else {
+        date_str = params.year + "-" + params.month
+    }
+
+    if(params.month <= 10) {
+        prev_date_str = `${params.year}-0${params.month - 1}`
+    } else {
+        prev_date_str = `${params.year}-${params.month - 1}`
+    }
+
+    //Pretty sure I'm technically selecting everything from the current and previous month, which is not super efficient, but it is what it is
     let { data: events, error } = await supabase
     .from('events')
     .select('*')
-    .ilike('event_date', `%${date_str}%`)
+    .or(`event_date.ilike.%${date_str}%, event_date.ilike.%${prev_date_str}%`)
     .order('event_date', { ascending: true })
 
     if(error)
