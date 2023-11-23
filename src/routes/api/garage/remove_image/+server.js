c880928235f77eb250d63e66f92ea89f7fa20afb
UnitedPuggs <edpoulsonv@hotmail.com>
2023-11-22 16:44:28 -0800

    fix: deleting garage images actually removes them from the bucket

--- src/routes/api/garage/remove_image/+server.js
+++ src/routes/api/garage/remove_image/+server.js
@@ -4,22 +4,25 @@ export async function DELETE({ request, locals }) {
     const session = await locals.getSession()
     const supabase = await client(session)
     const { id, url, imgs } = await request.json();
+
+    let stringy_url = [String(url.substring(80))]
+
+    console.log(stringy_url)
     
-    const { delete_data, delete_error } = await supabase
+    const { delete_error } = await supabase
     .storage
     .from('garage_photos')
-    .remove([url])
+    .remove(stringy_url)
 
     if(delete_error)
         return new Response(JSON.stringify(delete_error))
 
     const fixed_array = imgs.filter(e => e !== url)
     
-    const { data, error } = await supabase
+    const { error } = await supabase
     .from('garage_vehicle_info')
     .update({ image_urls: fixed_array })
     .eq('id', id)
-    .select()
 
     if(error)
         return new Response(JSON.stringify(error))
