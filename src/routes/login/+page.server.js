a5c7b089cb93773c2224f773fab9f3514ae31491
UnitedPuggs <edpoulsonv@hotmail.com>
2024-10-27 20:51:53 -0700

    fix(?): auth blew the fuck up

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -3,7 +3,6 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ parent }) {
     const { session } = await parent()
     if(session) {
-        //console.log(session)
         redirect(302, "/login/new-user");
     }
 
