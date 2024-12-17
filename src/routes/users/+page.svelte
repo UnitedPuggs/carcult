31efba741836c6b1020e5ffe72c6a0069d827b28
UnitedPuggs <edpoulsonv@hotmail.com>
2024-12-16 18:16:35 -0800

    feat: move users to form actions

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,21 +1,19 @@
 <script>
     import UserCard from "$lib/users/UserCard.svelte";
 
+    let { data } = $props();
+
     let username_search = $state();
     let users_found = $state([]);
 
+    $effect(() => {
+        if (data.users) {
+            users_found = data.users;
+        }
+    })
+
     //by default we assume users are found
     let found_flag = $state(true);
-
-    async function search_users() {
-        const req = await fetch(`/api/users/search_users?username=${username_search}`)
-        const res = await req.json();
-        users_found = res.users;
-        users_found = users_found;
-
-        if(users_found.length == 0)
-            found_flag = false;
-    }
 </script>
 
 <svelte:head>
@@ -23,8 +21,8 @@
 </svelte:head>
 
 <div class="flex flex-col mt-10 mb-2">
-    <form class="flex flex-row justify-center items-center gap-2 border-2 border-black rounded-lg offset-box p-2 w-fit mx-auto" onsubmit={search_users}>
-        <input type="search" class="text-black p-1 border border-black rounded-md shadow" placeholder="find a carcultist" bind:value={username_search}>
+    <form class="flex flex-row justify-center items-center gap-2 border-2 border-black rounded-lg offset-box p-2 w-fit mx-auto" method="GET">
+        <input name="username" type="search" class="text-black p-1 border border-black rounded-md shadow" placeholder="find a carcultist" bind:value={username_search}>
         <input type="submit" class="btn cursor-pointer hover:translate-y-0 -translate-y-1" value="&#128269">
     </form>
     {#if users_found.length > 0}
